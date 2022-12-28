6a17f6c4e3e2d7d114284d82819fc0d7d3981965
catsia <katya.lemesh@gmail.com>
2022-12-28 19:35:33 +0300

    Implement ArrayListProductDaoTest

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,8 +3,11 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -20,5 +23,31 @@ public class ArrayListProductDaoTest
         assertFalse(productDao.findProducts().isEmpty());
     }
 
+    @Test
+    public void testSaveExistingProduct(){
+        Currency currency = Currency.getInstance("USD");
+        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+        productDao.save(product);
+        product.setCode("test-product");
+        productDao.save(product);
+        assertEquals("test-product", productDao.getProduct(product.getId()).getCode());
+    }
+    @Test
+    public void testSaveProduct(){
+        Currency currency = Currency.getInstance("USD");
+        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+        productDao.save(product);
+        assertEquals(product.getCode(), productDao.getProduct(product.getId()).getCode());
+    }
 
+    @Test
+    public void testDeleteProduct(){
+        Currency currency = Currency.getInstance("USD");
+        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+        productDao.save(product);
+        productDao.delete(product.getId());
+        assertThrows(NoSuchElementException.class, () ->{
+           productDao.getProduct(product.getId());
+        });
+    }
 }
