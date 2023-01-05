69dc746750405c8a36ccfc249c0c014f3dc94f38
catsia <katya.lemesh@gmail.com>
2023-01-05 14:42:57 +0300

    Added coverage

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -5,52 +5,87 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.Assert.*;
 
-public class ArrayListProductDaoTest
-{
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
+    Product product;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
-    }
-    @Test (expected = NoSuchElementException.class)
-    public void testGetProducts(){
         Currency currency = Currency.getInstance("USD");
-        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+        product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+    }
+
+    @Test
+    public void testGetExistingProducts() {
         productDao.save(product);
         assertEquals("test", productDao.getProduct(product.getId()).getCode());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGetNonExistingProducts() {
+        productDao.save(product);
         productDao.getProduct(product.getId() + 1);
     }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGetProductNullId() {
+        productDao.getProduct(null);
+    }
+
     @Test
     public void testFindProductsHasResults() {
         assertFalse(productDao.findProducts().isEmpty());
     }
+
     @Test
-    public void testSaveExistingProduct(){
-        Currency currency = Currency.getInstance("USD");
-        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+    public void testFindProductsReturnsNotNullPrice() {
+        List<Product> products = productDao.findProducts();
+        for (Product tempProduct : products) {
+            assertNotNull(tempProduct.getPrice());
+            assertTrue(tempProduct.getStock() > 0);
+        }
+    }
+
+    @Test
+    public void testSaveExistingProduct() {
         productDao.save(product);
         product.setCode("test-product");
         productDao.save(product);
         assertEquals("test-product", productDao.getProduct(product.getId()).getCode());
     }
+
     @Test
-    public void testSaveProduct(){
-        Currency currency = Currency.getInstance("USD");
-        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+    public void testSaveNewProduct() {
         productDao.save(product);
         assertEquals(product.getCode(), productDao.getProduct(product.getId()).getCode());
     }
-    @Test (expected = NoSuchElementException.class)
-    public void testDeleteProduct(){
-        Currency currency = Currency.getInstance("USD");
-        Product product = new Product("test", "HTC EVO Shift 4G", new BigDecimal(320), currency, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg");
+
+    @Test(expected = NullPointerException.class)
+    public void testSaveNullProduct() {
+        productDao.save(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testDeleteExistingProduct() {
         productDao.save(product);
         productDao.delete(product.getId());
         productDao.getProduct(product.getId());
+    }
+
+    @Test
+    public void testDeleteNonExistingProduct() {
+        productDao.delete(Long.MAX_VALUE);
+        productDao.delete(Long.MIN_VALUE);
+    }
+
+    @Test
+    public void testDeleteProductNullId() {
+        productDao.delete(null);
     }
 }
