6c1177fdfb1ba1c5dc3b10202c5f552f6c8d3f7e
catsia <katya.lemesh@gmail.com>
2023-01-06 14:52:28 +0300

    Added {} in if statement

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -39,7 +39,9 @@ public class ArrayListProductDao implements ProductDao {
     @Override
     public void save(Product product) {
         synchronized (lock) {
-            if (product == null) throw new IllegalArgumentException();
+            if (product == null) {
+                throw new IllegalArgumentException();
+            }
             if (product.getId() != null) {
                 delete(product.getId());
                 products.add(product);
